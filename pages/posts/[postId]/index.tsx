b7d0d1562b33a738118b1f8a2a2d452f5cf4980d
soumyaRauth <shoummorauth@live.co.uk>
2024-11-16 04:32:54 +0600

    code optimized

--- pages/posts/[postId]/index.tsx
+++ pages/posts/[postId]/index.tsx
@@ -17,9 +17,9 @@ interface Params extends ParsedUrlQuery {
   postId: string;
 }
 
-const PostDetail: NextPage = ({
-  post,
-}: InferGetStaticPropsType<typeof getStaticProps>) => {
+const PostDetail: NextPage<{
+  post: InferGetStaticPropsType<typeof getStaticProps>;
+}> = ({ post }) => {
   return (
     <>
       <p>{post.id}</p>
@@ -33,9 +33,11 @@ export const getStaticPaths: GetStaticPaths = async () => {
   const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
   const posts: Post[] = await response.json();
 
-  const allPostIds = await posts.map((post) => {
-    return { params: { postId: post.id.toString() } };
-  });
+  const allPostIds = posts.map((post) => ({
+    params: {
+      postId: post.id.toString(),
+    },
+  }));
 
   return {
     paths: allPostIds,
@@ -47,17 +49,25 @@ export const getStaticProps: GetStaticProps = async (
   context: GetStaticPropsContext
 ) => {
   const { postId } = context.params as Params;
-
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${postId}`
-  );
-  const post: Post = await response.json();
-
-  return {
-    props: {
-      post: post,
-    },
-  };
+  try {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${postId}`
+    );
+    const post: Post = await response.json();
+    if (!post) {
+      return { notFound: true };
+    }
+    return {
+      props: {
+        post: post,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching post:", error);
+    return {
+      notFound: true, // Return 404 if fetch fails
+    };
+  }
 };
 
 export default PostDetail;
